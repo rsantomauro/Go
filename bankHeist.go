9ea34e0b6460e2438bddd5a317d7f5381e84b31f
Rodrigo Santomauro <rodrigoolema@outlook.com>
2022-05-19 22:46:32 -0300

    first commit

---
+++ bankHeist.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func main() {
+  rand.Seed(time.Now().UnixNano())
+  isHeistOn := true
+  eludedGuards := rand.Intn(100)
+
+  if (eludedGuards >= 50){
+    fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
+  } else {
+    isHeistOn = false
+    fmt.Println("Plan a better disguise next time?")
+  }
+
+  openedVault := rand.Intn(100)
+
+  if isHeistOn && (openedVault >= 70){
+    fmt.Println("Grab and GO!")
+  } else if (isHeistOn) && (openedVault <= 70){
+    isHeistOn=false
+    fmt.Println("the vault can’t be opened")
+  }
+
+  leftSafely := rand.Intn(5)
+
+  if isHeistOn {
+    switch leftSafely {
+      case 0:
+        isHeistOn = false
+        fmt.Print("Busted")
+      case 1:
+        isHeistOn = false 
+        fmt.Print("Turns out vault doors don't open from the inside...")
+      case 2:
+        isHeistOn = false 
+        fmt.Print("Turns out vault doors don't open from the inside...")
+      default:
+        fmt.Print("Start the getaway car!")
+    }
+  }
+
+  if isHeistOn {
+    amtStolen :=10000 + rand.Intn(1000000)
+    fmt.Print("The amout: ", amtStolen)
+  }
+}
